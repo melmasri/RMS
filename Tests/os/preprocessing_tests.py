6286a85086177b207dd562b2821049d5812a61cf
Oscar Alberto Quijano Xacur <oscar@oficina>
2015-09-21 21:56:45 -0400

    Finished validation step and log

--- Tests/os/preprocessing_tests.py
+++ Tests/os/preprocessing_tests.py
@@ -12,18 +12,22 @@ class questionnaire_test(questionnaire):
         if self.edit_mode:
             sheet=self.wb.sheets()[0]
             nadm1= sheet.cell(4,1).value
+            #print("nadm1: {0}\n".format(nadm1))
         else:
             administrative_divisions_variables = pre_vars['fixed_sheets']['Administrative divisions']
             sheet = self.wb.sheet_by_name('Administrative divisions')
             nadm1 = sheet.cell( *indexes( administrative_divisions_variables['adm1_number'][0]  )   ).value
         if( (type(nadm1) == int or type(nadm1) == float) and int(nadm1)==nadm1 and nadm1 > 0 ):
+            self.print_log("Number of administrative divisions: {0}\n".format(nadm1))
             return(True)
         else:
+            self.print_log("Error: Wrong value for number of administrative divisions.\n")
             return(False)
         
     def check_adm1_names(self):
         """Checks that the region names are filled.
         """
+        administrative_divisions_variables = pre_vars['fixed_sheets']['Administrative divisions']
         sheet=self.wb.sheet_by_name('Administrative divisions')
         id_start_coordinates=indexes( administrative_divisions_variables['id_start'][0])
         regions_names=sheet.col_values(id_start_coordinates[1]+1,\
@@ -33,32 +37,56 @@ class questionnaire_test(questionnaire):
                                  ##The following line tests wether it is not empty and different that ...
                                  map( lambda region_name: region_name and region_name != "..." , 
                                       regions_names))
+        if (all_regions_good):
+            self.print_log("Administrative divisions:\n")
+            for region in regions_names:
+                self.print_log("                  {}\n".format(region))
+            self.print_log("\n")
+        else:
+            self.print_log("Error: Empty names for administrative divisions.")
         return(all_regions_good)
 
     def check_reference_year(self):
+        """Checks that the reference year is filled with the right value"""
         if (self.edit_mode):
-            pass # or True?
+            return(True)
         else:
             front_page_variables=pre_vars['fixed_sheets']['Front Page']
             cellname=front_page_variables['academic_year'][0]
             sheet=self.wb.sheet_by_name('Front Page')
             academic_year_value=sheet.cell(*indexes(cellname)).value
-            return (academic_year_value >0 and academic_year_value <=2015 )
+            test_value=academic_year_value >0 and academic_year_value <=2015
+            if (test_value):
+                self.print_log("Reference year: {0}\n".format(academic_year_value))
+            return(test_value)
 
     def check_country_name(self):
+        """Checks if the country name is filled"""
         if (self.edit_mode):
-            pass # or True?
+            return(True)
         else:
             front_page_variables=pre_vars['fixed_sheets']['Front Page']
             cellname=front_page_variables['country_name'][0]
             sheet=self.wb.sheet_by_name('Front Page')
-            return( sheet.cell_type( *indexes(cellname) ) == front_page_variables['country_name'][1] )
+            country_name=sheet.cell(*indexes(cellname)).value
+            test_value=sheet.cell_type( *indexes(cellname) ) == front_page_variables['country_name'][1]
+            if (test_value):
+                self.print_log("Country name is filled: {0}\n".format(country_name))
+            else:
+                seld.print_log("Error: Country name is not filled or has a wrong format.")
+            return(test_value)
+        
     def check_number_of_sheets(self):
         if (self.edit_mode):
-            pass
+            return(True)
         else:
             if pre_vars['nsheets']==self.wb.nsheets:
                 self.print_log("The correct number of sheets"+ "({})".format(self.wb.nsheets) +"has been submited.\n")
+                return(True)
+            else:
+                self.print_log("Error: Incorrect number of sheets submited\n")
+                return(False)
+            
     def check_edited_configuration_part(self):
         """This functions checks that the table in the top left corner of
         the sheet exists in an edite questionnaire.
@@ -71,9 +99,14 @@ class questionnaire_test(questionnaire):
             test1= configuration_names == ['Country', 'CO_CODE', 'Year', 'Data', 'No.ADM', 'Series']
             # test2 os to check that the values are not empty (May be it could be improved). 
             test2=reduce( lambda x,y: x and y, configuration_values)
-            return(test1 and test2)
+            test_value=test1 and test2
+            if (test_value):
+                self.print_log("Configuration section of edited questionnaire is properly filled\n")
+            else:
+                self.print_log("Error: Configuration section has wrong values.")
+            return(test_value)
         else:
-            pass
+            return(True)
 
     def check_one_value(self,value):
         """Checks that value (the argument) is proper.
@@ -116,25 +149,25 @@ class questionnaire_test(questionnaire):
             ## We read the country value.
             meter_value_country=sheet.cell( meter_starting_coordinates[0]+self.nadm1+1,\
                                             meter_starting_coordinates[1]).value
-            self.print_log("{}".format(meter_values))
             meter_values=[meter_value_country]+meter_values
             # The following will be zero if there is at least one
             # error. 1 if everything is ok and 2 id there is at least one A or N.
             column_test=reduce( lambda x,y: x * y , map( self.check_one_value,meter_values ) ) 
             if(not column_test):
-                self.print_log("Column {0} in table {1} has improper values.\n".format(col_number,table))
+                self.print_log("Error: Column {0} in table {1} has improper values.\n".format(col_number,table))
             elif(column_test==2):
-                self.print_log("Column {0} in table {1} has at least one A or N.\n".format(col_number,table))
+                self.print_log("Error: Column {0} in table {1} has at least one A or N.\n".format(col_number,table))
             overall_test=overall_test and column_test
         return(overall_test)
-    def print_log_new(self,log_type=False,text_string):
+    
+    def print_log(self,text_string,log_type=False):
         """Puts the test in log and stdout.
 
         if log_type=True (the default), it writes to the validation log
         if log_type=False (the default), it writes to the error log
         """
         print(text_string,end='')
-        if (log_type):            
+        if (not log_type):            
             self.validation_log_file.write(text_string)
             self.validation_log_file.flush()
             os.fsync(self.validation_log_file.fileno())
@@ -147,9 +180,26 @@ class questionnaire_test(questionnaire):
 
     def validation(self):
         check_variables=pre_vars["Checking sheet"]
-        self.print_log("----------"+"Date: "+datetime.datetime.now().strftime("%B %d, %Y")+"----------\n\n\n")
+        self.print_log("----------"+"Date: "+datetime.datetime.now().strftime("%B %d, %Y")+"----------\n")
+        self.print_log("VALIDATION STEP\n\n")
+        if (not self.edit_mode):
+            self.print_log("Original questionnaire submited with path:\n")
+            self.print_log(self.excel_file+"\n\n")
+            administrative_divisions_variables=pre_vars['fixed_sheets']['Administrative divisions']
+        else:
+            self.print_log("Edited questionnaire submited with path:\n")
+            self.print_log(self.excel_file+"\n\n")
 
-    def init(self):
+        nadm1_test=self.check_nadm1()
+        adm1_names_test=self.check_adm1_names()
+        reference_year_test=self.check_reference_year()
+        country_name_test=self.check_country_name()
+        number_of_sheets_test=self.check_number_of_sheets()
+        edited_configuration_part_test=self.check_edited_configuration_part()
+        values_test=self.check_values()        
+        return ( nadm1_test and adm1_names_test and reference_year_test and country_name_test and number_of_sheets_test and edited_configuration_part_test and values_test )
+
+    def init2(self,log_folder="/tmp/log"):
         self.validation_log_file=open( log_folder + "/{}".format(self.country_name) + "_"+datetime.datetime.now().strftime("%y-%m-%d-%H-%M")+"_validation.log",'a')
         self.error_log_file=open( log_folder + "/{}".format(self.country_name) + "_"+datetime.datetime.now().strftime("%y-%m-%d-%H-%M")+"_error.log",'a')
             
@@ -157,4 +207,5 @@ excel_file="Export/Lao People's Democratic Republic_2012_All_REP.xlsx"
 database="Database/Prod.db"
 
 x=questionnaire_test(excel_file,database)
-#x.check_nadm1()
+x.init2()
+x.validation()
