69aabf6dd928dfc5dc0fa473b46a8f6270177c68
melmasri <elmasri.m@gmail.com>
2015-12-10 23:41:01 -0500

    Ed Attain

--- Libraries/rmindicators.py
+++ Libraries/rmindicators.py
@@ -1,6 +1,11 @@
 import sqlite3,re
 import sys, getpass, os, csv
 import csv
+
+##################################################
+#### table Algebra for sum/div/prod operations
+##################################################
+
 ## Default algebra tables
 algebra_sum = {'a': {'a': 'a', 'm': 'm', 'n': 'n','x':'x', '':''},
                'm': {'a': 'm', 'm': 'm', 'n': 'm','x':'m', '':'m'},
@@ -32,7 +37,7 @@ def read_algebra():
 
 def arrange_algebra_dist(data, op= 'Sum'):
     """ Arranging algebra from a csv file read in data based os an operation (op) in a dictionary as seen in the global variables.
-        The left corner of the talbe in the csv file should be the op, case sensitive
+        The left corner of the table in the csv file should be the op, case sensitive
     """
     result = {}
     for row in data:
@@ -42,6 +47,14 @@ def arrange_algebra_dist(data, op= 'Sum'):
             pass
         result[key] = row
     return(result)
+
+## Reads new algebra tables
+read_algebra()
+##################################################
+
+##################################################
+##### Generic mathematical operators
+##################################################
 
 def sum_list(x):
     """ Sums a single list recursively """
@@ -67,7 +80,7 @@ def sum(x,y):
     global algebra_sum
     algeb = algebra_sum[x[1]][y[1]]
     if algeb =='':
-        return([x[0] + y[0],''])
+        return([(x[0] or 0 ) + (y[0] or 0),''])
     return(['',algeb])
 
 def prod(x,y):
@@ -86,7 +99,7 @@ def prod(x,y):
     global algebra_prod
     algeb = algebra_prod[x[1]][y[1]]
     if algeb =='':
-        return([x[0]*y[0],''])
+        return([(x[0] or 0)*(y[0] or 0),''])
     return(['',algeb])
 
 def div(x,y):
@@ -105,17 +118,85 @@ def div(x,y):
     global algebra_div
     algeb = algebra_div[x[1]][y[1]]
     if algeb =='':
-        return([x[0]/y[0],''])
+        return([(x[0] or 0)/(y[0] or 0),''])
     return(['',algeb])
 
-## Reads new algebra tables
-read_algebra()
+def op2col(col1, col2, op):
+    """ Returns the op(sum/div/prod) of two columns"""
+    return  list(map(lambda x,y: op(x, y), col1, col2))
+##################################################
+
+##################################################
+#### Highest level of EdAttain
+##################################################
+def highestEA_list(level, indic, offset=0):
+    """ Returns the ratio of the highest level of EdAttain given the level
+        offset is the year, 0 for current year assigned by indic, -1 is N-1 year
+        indic an instance of indicator class"""
+    ac_pop = 'Pop.Ag0t99'
+    if len(level) ==1:
+        a = indic.column_operation([level[0], offset], [ac_pop,offset], lambda x,y: div(x,y)) 
+    else:    
+        b = list(map(lambda x: indic.column_operation([x, offset], [ac_pop,offset], lambda x,y: div(x,y)), level))
+        a = reduce(lambda x,y: op2col(x, y, sum), b)
+    return(a)
+
+def highestEA(schoolLevel,  indic, offset=0):
+    """ 
+    Returns a dictionary of the indicator EA(x)PT.(y).(z), where
+    x is 2m, 3, 4, or 5p as the attainment levels
+    y is the parameter schoolLevel defined by ['1', '2.GPV', '3.GPV', '23.GPV']
+    z is ['', '.Pu', '.Pr']
+    
+    The key of the dictionary is the indicator name EA(x)PT.(y).(z) and the value is
+    a list of calculated figures for each region. 
+    
+    indic is an instance of indicator class,
+    offset is the year offset, 0 as current year defined in indicator class indic, and -1, is the N-1 year.
+    """
+    if schoolLevel not in ['1', '2.GPV', '3.GPV', '23.GPV']:
+        print("The only schoolLevel allowed is ['1', '2.GPV', '3.GPV', '23.GPV'] ")
+        return
+    
+    dic_level = {'2m':['T.x.EA.2m'] , '3':['T.x.EA.3'], '4': ['T.x.EA.4'], '5p':['T.x.EA.5', 'T.x.EA.6','T.x.EA.7','T.x.EA.8'], 'uk':['T.x.EA.uk'] }
+    typeSchool  = ['', '.Pu', '.Pr']
+    EA_dic = {}
+    for key, value in dic_level.items():
+        for t in typeSchool:
+            name = 'EA' + key + 'PT.' + s + t
+            level_list = list(map(lambda x: x.replace('x', s+t), dic_level[key]))
+            EA_dic.update({name:highestEA_list(level_list, indic, offset)})
+    return EA_dic
+
+##################################################
+##################################################
+
+def EA_all(indic, offset=0):
+    """
+    Calculates the highest EdAttain for for levels  ['1', '2.GPV', '3.GPV', '23.GPV']
+    returns a dictionary of dictionaries. The first level is of size 4, as for each level
+    and the second levels is a dictionary creates by highestEA_list  as:
+    
+    A dictionary of the indicator EA(x)PT.(y).(z), where
+    x is 2m, 3, 4, or 5p as the attainment levels
+    y is the parameter schoolLevel defined by ['1', '2.GPV', '3.GPV', '23.GPV']
+    z is ['', '.Pu', '.Pr']
+    
+    The key of the dictionary is the indicator name EA(x)PT.(y).(z) and the value is
+    a list of calculated figures for each region. 
+    """
+    ea = {} 
+    isced = ['1', '2.GPV', '3.GPV', '23.GPV']
+    for s in isced:
+        ea.update({s : highestEA(s,  indic, offset) })
+    return(ea)    
+
 
 def mean_age(indic, codes):
     """ Calculates the mean age for a given list of codes"""
     midpoint = [[20,''], [24,''], [34,''], [44,''],[54,''], [65,'']]
     ac_pop = 'Pop.Ag0t99'
-    temp = list(map(lambda z,v: indic.column_operation([z,0], [ac_pop, 0], lambda x,y:      prod(div(x,y), v)),codes, midpoint))
+    temp = list(map(lambda z,v: indic.column_operation([z,0], [ac_pop, 0], lambda x,y:   prod(div(x,y), v)),codes, midpoint))
     temp = list(map(sum_list, temp))
     return(temp)
 
